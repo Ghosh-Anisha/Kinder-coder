d8905524c44fe5c15a4e2b63425c28e14a714bf6
gayathrisunil <56356145+gayathrisunil@users.noreply.github.com>
2021-01-23 12:16:56 +0530

    First commit
    Added landing page for website

---
+++ index.js
@@ -0,0 +1,16 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+const signInBtn = document.getElementById('sign-in-btn');
+signInBtn.addEventListener('click', ()=> {
+	console.log('I was clicked');
+});
